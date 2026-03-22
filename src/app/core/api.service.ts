c0bd8a3140bf8dfa4ff1163f4307d802b218f3c9
SzMaciek <brakmaciejko@gmail.com>
2026-03-22 15:01:41 +0100

    feat(core): add API service to fetch orders, instruments and contract types
    Also create proper models returned by Geeksoft public API for future
    usage inside table

---
+++ src/app/core/api.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import {
+  OrderItem,
+  OrderDataResponse,
+  InstrumentItem,
+  ContractTypeItem,
+} from './models/table-data.models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GeekSoftApiService {
+  private readonly httpClient = inject(HttpClient);
+
+  private readonly BASE_API_URL = 'https://geeksoft.pl/assets/2026-task/';
+
+  getOrders(): Observable<OrderItem[]> {
+    return this.httpClient
+      .get<OrderDataResponse>(`${this.BASE_API_URL}order-data.json`)
+      .pipe(map(({ data }) => [...data]));
+  }
+
+  getInstruments(): Observable<InstrumentItem[]> {
+    return this.httpClient.get<InstrumentItem[]>(
+      `${this.BASE_API_URL}instruments.json`,
+    );
+  }
+
+  getContractTypes(): Observable<ContractTypeItem[]> {
+    return this.httpClient.get<ContractTypeItem[]>(
+      `${this.BASE_API_URL}contract-types.json`,
+    );
+  }
+}
