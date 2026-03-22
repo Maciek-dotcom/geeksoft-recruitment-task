897f3a890433294cacca30ff6b9474221e005f14
SzMaciek <brakmaciejko@gmail.com>
2026-03-22 17:20:20 +0100

    feat(dashboard): add base dashboard component and use it in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TradingDashboardComponent } from './dashboard/trading-dashboard/trading-dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [TradingDashboardComponent],
 })
 export class AppComponent {
   title = 'GeeksoftTrading';
