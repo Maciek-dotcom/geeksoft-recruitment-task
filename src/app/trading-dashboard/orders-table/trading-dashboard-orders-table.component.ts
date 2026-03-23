1580e09766d6828090f4ffc5a03aa535ea7dca27
SzMaciek <brakmaciejko@gmail.com>
2026-03-23 11:45:21 +0100

    refactor: do not handle notifications in store
    In order not to mix concerns, the store should not be responsible state.
    Other side effects (like notifications) should be handled in other
    layer of app

--- src/app/trading-dashboard/orders-table/trading-dashboard-orders-table.component.ts
+++ src/app/trading-dashboard/orders-table/trading-dashboard-orders-table.component.ts
@@ -2,6 +2,7 @@ import { DatePipe } from '@angular/common';
 import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { SmartDecimalPipe } from '../../core/pipes/smart-decimal.pipe';
 import { TradingDashboardOrdersTableStore } from '../trading-dashboard-order.store';
+import { NotificationService } from '../../core/services/notifications.service';
 
 @Component({
   selector: 'app-trading-dashboard-orders-table',
@@ -17,13 +18,19 @@ export class TradingDashboardOrdersTableComponent {
 
   protected readonly isLoading = this.store.loading;
 
+  private readonly notificationsService = inject(NotificationService);
+
   onRemoveGroup(event: Event, symbol: string): void {
     event.stopPropagation(); // do not toggle expansion when clicking the remove button
-    this.store.removeGroup(symbol);
+    const result = this.store.removeGroup(symbol);
+
+    if (result) this.notificationsService.closePosition(result.removedIds);
   }
 
   onRemoveOrder(event: Event, orderId: number): void {
     event.stopPropagation();
-    this.store.removeOrder(orderId);
+    const result = this.store.removeOrder(orderId);
+
+    if (result) this.notificationsService.closePosition(result.removedIds);
   }
 }
